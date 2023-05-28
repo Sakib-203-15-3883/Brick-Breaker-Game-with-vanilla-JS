0d0a11708b005d0adfda2122b8e52a3cf6af4f1e
sakibul islam <sakibul15.3883@gmail.com>
2023-05-28 17:56:31 +0600

    ball , brick and level defines

--- game.js
+++ game.js
@@ -77,3 +77,170 @@ console.log(PADDLE_STROKE);
 //This constant determines the speed at which the paddle moves horizontally. It represents the number of pixels the paddle will shift when the player controls its movement using the arrow keys or any other input method.
 
 const PADDLE_SPEED = 8;
+
+// paddle configs
+
+// here , we use object literal notation to define the properties of the paddle.It allows for easy access and manipulation of the paddle's properties throughout the game logic.
+
+let paddle = {
+  //This line determines the initial x-coordinate of the paddle. It positions the paddle horizontally at the center of the canvas by subtracting half of the paddle's width (PADDLE_WIDTH) from half of the canvas's width (canvas.width / 2).
+
+  x: canvas.width / 2 - PADDLE_WIDTH / 2,
+
+  //This line determines the initial y-coordinate of the paddle. It positions the paddle vertically at the bottom of the canvas by subtracting the paddle's height (PADDLE_HEIGHT) and the margin between the paddle and the bottom of the screen (PADDLE_MARGIN_BOTTOM) from the canvas's height (canvas.height).
+
+  y: canvas.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+
+  // here, we assign property value
+
+  width: PADDLE_WIDTH,
+  height: PADDLE_HEIGHT,
+  speed: PADDLE_SPEED,
+};
+
+// Ball configs
+
+//this code represents the ball object. The ball is the object that moves around the game screen, bouncing off the walls, bricks, and the paddle. The code uses an object literal notation to define the properties of the ball.
+
+//This constant determines the radius of the ball. It specifies the size of the ball as a circular object.
+
+const BALL_RADIUS = 10;
+
+//This constant determines the initial speed of the ball. It specifies how fast the ball moves in pixels per frame or time unit.
+
+const BALL_SPEED = 3;
+const BALL_COLOR = "#00F19D";
+const BALL_STROKE = "#fff ";
+const BALL_STROKE_WIDTH = 5;
+
+let ball = {
+  //This line determines the initial x-coordinate of the ball. It positions the ball horizontally at the center of the canvas by assigning half of the canvas's width (canvas.width / 2) to the x property of the ball object.
+
+  x: canvas.width / 2,
+
+  //his line determines the initial y-coordinate of the ball. It positions the ball vertically just above the paddle by subtracting the ball's radius (BALL_RADIUS) and an additional pixel (-1) from the y-coordinate of the paddle (paddle.y).
+
+  y: paddle.y - BALL_RADIUS - 1,
+  radius: BALL_RADIUS,
+
+  //This line indicates whether the ball is currently moving or not. It sets the initial value of the moving property to false, indicating that the ball is initially stationary.
+
+  moving: false,
+
+  //This line sets the initial speed of the ball in the horizontal (x) direction. It assigns the value of 0 to the speedX property of the ball object. This indicates that the ball initially has no horizontal movement.
+
+  speedX: 0,
+
+  //The negative value indicates that the ball moves upward (opposite to the positive y-axis direction) at a speed determined by BALL_SPEED.
+
+  speedY: -BALL_SPEED,
+};
+
+//Brick configs
+
+//This line sets the height of each brick to 20 pixels by assigning the value 20 to the constant BRICK_HEIGHT.
+
+const BRICK_HEIGHT = 20;
+
+//This line sets the width of each brick to 75 pixels by assigning the value 75 to the constant BRICK_WIDTH.
+
+const BRICK_WIDTH = 75;
+
+//This line sets the vertical offset between rows of bricks. It determines the distance between each row of bricks by assigning the value 30 to the constant BRICK_OFFSET_Y.
+
+const BRICK_OFFSET_Y = 30;
+
+//This line sets the horizontal distance between each brick in a row. It assigns the value 5 to the constant BRICK_DISTANCE_X, which determines the spacing between bricks in the x-axis.
+
+const BRICK_DISTANCE_X = 5;
+
+// This line sets the vertical distance between each brick in a column. It assigns the value 20 to the constant BRICK_DISTANCE_Y, which determines the spacing between bricks in the y-axis.
+
+const BRICK_DISTANCE_Y = 20;
+
+//These lines define various colors used to fill and stroke the different types of bricks. Each constant represents a specific color code.
+
+const NORMAL_BRICK_FILL = "#D2E809";
+const NORMAL_BRICK_STROKE = "#847101";
+const MID_STRONG_BRICK_FILL = "#80097e";
+const MID_STRONG_BRICK_STROKE = "#3c003b";
+const STRONG_BRICK_FILL = "#00439f";
+const STRONG_BRICK_STROKE = "#001351";
+const SUPER_STRONG_BRICK_FILL = "#f78501";
+const SUPER_STRONG_BRICK_STROKE = "#a75501";
+
+//This line sets the stroke width for the bricks to 2.5 pixels. It assigns the value 10 to the constant BRICK_STROKE_WIDTH.
+
+const BRICK_STROKE_WIDTH = 10;
+
+//it  defines the brick object, which represents an individual brick. It has properties such as x, y, height, width, and lifes
+
+let brick = {
+  //set the initial position of the brick to the top-left corner of the canvas.
+
+  x: 0,
+  y: 0,
+  height: BRICK_HEIGHT,
+  width: BRICK_WIDTH,
+
+  //epresents the number of lives or hits required to break the brick. The initial value is set to 0, and it will change dynamically to indicate the strength of the brick.
+
+  lifes: 0,
+};
+
+//This line initializes an empty array called bricks. This array will store multiple brick objects, representing the collection of bricks present in the game.
+
+let bricks = [];
+
+//==================== LEVELS =====================
+
+//This code defines six levels for the game, each represented by a 2-dimensional array.
+
+//level 1 represents the first level. It has two rows, and each row contains eight bricks. The value 1 in the array represents a normal brick with one life or hit required to break it.
+
+const level1 = [
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+// level2 represents the second level. It also has two rows and eight bricks in each row. The value 2 in the array represents a slightly stronger brick that requires two hits to break, while the value 1 represents a normal brick.
+
+const level2 = [
+  [2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+//level3 has three rows with eight bricks in each row. The values 3, 2, and 1 represent bricks with increasing strength, where 3 requires three hits to break, 2 requires two hits, and 1 represents a normal brick.
+
+const level3 = [
+  [3, 3, 3, 3, 3, 3, 3, 3],
+  [2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+//level4 is similar to level3 but with an additional row of 2-strength bricks.
+
+const level4 = [
+  [3, 3, 3, 3, 3, 3, 3, 3],
+  [2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2],
+];
+
+//level5 introduces a pattern with different brick types. The values 3 and 2 form a pattern of alternating bricks, creating a design with different strengths.
+
+const level5 = [
+  [3, 1, 1, 3, 3, 1, 1, 3],
+  [2, 3, 3, 2, 2, 3, 3, 2],
+  [2, 3, 3, 2, 2, 3, 3, 2],
+  [3, 1, 1, 3, 3, 1, 1, 3],
+];
+
+//level6 has a more complex pattern with multiple types of bricks. The value 4 represents a stronger brick that requires four hits to break, while the other values (3, 2, and 1) represent bricks of decreasing strength.
+
+const level6 = [
+  [3, 3, 3, 3, 3, 3, 3, 3],
+  [1, 1, 4, 4, 4, 4, 1, 1],
+  [2, 2, 4, 4, 4, 4, 2, 2],
+  [1, 1, 4, 4, 4, 4, 1, 1],
+  [3, 3, 3, 3, 3, 3, 3, 3],
+];
